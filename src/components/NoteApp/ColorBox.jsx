363bfd852f40499a03702fc18d423894c843ee6b
Hossein <hhasnabadi@yahoo.com>
2024-12-31 02:04:05 +0330

    noteapp

---
+++ src/components/NoteApp/ColorBox.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class ColorBox extends Component {
+    inputColorHandler(color){
+        this.props.onColor(color)
+    }
+
+    render() {
+        let {color}=this.props
+
+        return (
+            <div className='color-box' style={{backgroundColor:color}} onClick={this.inputColorHandler.bind(this,color)}>
+                
+            </div>
+        )
+    }
+}
